f0aa253ffe268b7175cf1379e79ac5a41cea16ac
rayanesal <salesrayane11@gmail.com>
2025-09-23 15:11:47 -0300

    refactor: sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,17 +2,18 @@ import { Link } from 'react-router-dom';
 
 export function Sidebar() {
   return (
-    <aside className="w-64 h-screen bg-gray-800 text-white p-5">
-      <h1 className="text-2xl font-bold mb-10">Leads</h1>
+    <aside className="w-64 h-screen bg-slate-900 text-slate-100 p-6">
+      <img src="leads_logo.svg" alt="Leads Logo" className="mt-10 w-32 mx-auto mb-10" />
+      <h1 className="text-2xl font-bold mb-4">Leads</h1>
       <nav>
         <ul>
-          <li className="mb-4">
-            <Link to="/" className="block p-2 rounded hover:bg-gray-700">
+          <li className="mb-2">
+            <Link to="/" className="block p-2 rounded hover:bg-slate-700">
               Captura de Leads
             </Link>
           </li>
           <li>
-            <Link to="/list" className="block p-2 rounded hover:bg-gray-700">
+            <Link to="/list" className="block p-2 rounded hover:bg-slate-700">
               Lista de Leads
             </Link>
           </li>
